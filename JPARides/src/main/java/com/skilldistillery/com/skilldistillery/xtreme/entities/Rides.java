ef634c2a227bb139dfa1215fea782936c1331cf6
Newlifedirection <craig.dreiling@gmail.com>
2019-08-02 15:42:24 -0600

    JPA stubbed

---
+++ JPARides/src/main/java/com/skilldistillery/com/skilldistillery/xtreme/entities/Rides.java
@@ -0,0 +1,67 @@
+package com.skilldistillery.com.skilldistillery.xtreme.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Rides {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String ridescol;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getRidescol() {
+		return ridescol;
+	}
+	public void setRidescol(String ridescol) {
+		this.ridescol = ridescol;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		result = prime * result + ((ridescol == null) ? 0 : ridescol.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Rides other = (Rides) obj;
+		if (id != other.id)
+			return false;
+		if (ridescol == null) {
+			if (other.ridescol != null)
+				return false;
+		} else if (!ridescol.equals(other.ridescol))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Rides [id=").append(id).append(", ridescol=").append(ridescol).append("]");
+		return builder.toString();
+	}
+	public Rides(int id, String ridescol) {
+		super();
+		this.id = id;
+		this.ridescol = ridescol;
+	}
+	public Rides() {
+		super();
+	}
+
+}
---
+++ JPARides/src/test/java/com/skilldistillery/com/skilldistillery/xtreme/entities/RidesTests.java
@@ -0,0 +1,51 @@
+package com.skilldistillery.com.skilldistillery.xtreme.entities;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class RidesTests {
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	private Rides rides;
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("RidesPU");
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+		emf.close();
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+		em= emf.createEntityManager();
+		rides = em.find(rides.getClass(), 1);
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		em.close();
+		rides = null;
+	}
+
+	@Test
+	void test_entity_mappings() {
+		assertNotNull(rides);
+		assertEquals("Hello", rides.getRidescol());
+		fail("Not yet implemented");
+	}
+
+}
